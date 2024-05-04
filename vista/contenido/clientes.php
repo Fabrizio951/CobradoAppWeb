db83b4a6ef512209e383d564fc02d0abaf8ef7ef
Fabrizio951 <fabrizio_zuniga2003@hotmail.com>
2024-05-03 22:44:09 -0500

    Iniciando CRUD de Clientes

--- vista/contenido/clientes.php
+++ vista/contenido/clientes.php
@@ -17,6 +17,61 @@
     <!-- /.content-header -->
     
     <!-- Main content -->
-    <section class="content">
-    </section>
+    <div class="content">
+      <div class="container-fluid">
+
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="card card-info">
+              <div class="card-header">
+                <h3 class="card-title">CRITERIOS DE BUSQUEDA</h3>
+                  <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                      <i class="fas fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-tool" id="btnLimpiarBusqueda">
+                        <i class="fas fa-times"></i>
+                    </button>
+                  </div> <!-- ./ end card-tools -->
+                </div> <!-- ./ end card-header -->
+              <div class="card-body">
+              </div> <!-- ./ end card-body -->
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-12">
+            <table id="tbl_clientes" class="table table-striped w-100 shadow">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>ID Cliente</th>
+                  <th>Nombre</th>
+                  <th>Apellido</th>
+                  <th>Dirección</th>
+                  <th>Teléfono</th>
+                  <th>Correo</th>
+                  <th>Estado</th>
+                  <th class="text-center">Opciones</th>
+                </tr>
+              </thead>
+              <tbody>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
     <!-- /.content -->
+
+    <script>
+    $(document).ready(function(){
+      var table;
+      table = $("#tbl_clientes").DataTable({
+        language:{
+          url: "https://cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json"
+        }
+      });
+    })
+    </script>
